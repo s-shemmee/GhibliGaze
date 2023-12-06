b2e574564f27a474c31549bfef108757b4e98a83
shemmee <chaimalamirin@gmail.com>
2023-12-06 22:56:20 +0000

    About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,9 @@
 export default function About() {
   return (
-    <section className="py-10 lg:py-10 bg-stone-100 dark:bg-gray-800" id="about-section">
+    <section
+      className="py-10 lg:py-20 bg-stone-100 dark:bg-gray-800"
+      id="about-section"
+    >
       <div className="max-w-screen-xl py-4 mx-auto lg:py-6 md:px-6">
         {/* Two-column layout with flex */}
         <div className="flex flex-wrap">
@@ -18,17 +21,37 @@ export default function About() {
               </div>
               {/* Main content paragraph */}
               <p className="px-4 mb-5 text-base leading-7 text-gray-500 dark:text-gray-400">
-                Hey there ૮ ˶ᵔ ᵕ ᵔ˶ ა! Let's dive into the magical world of Studio Ghibli 🌱, co-founded in '85 by the dynamic duo of animated wonders, <strong>Isao Takahata</strong> and <strong>Hayao Miyazaki</strong>.
+                Hey there ૮ ˶ᵔ ᵕ ᵔ˶ ა! Let's dive into the magical world of
+                Studio Ghibli 🌱, co-founded in '85 by the dynamic duo of
+                animated wonders, <strong>Isao Takahata</strong> and{" "}
+                <strong>Hayao Miyazaki</strong>.
                 <br />
-                This powerhouse has crafted twenty-two awe-inspiring films. Think box office gold—most of their creations soared to the top in Japan, and <strong>SPIRITED AWAY</strong>, directed by Miyazaki in 2001, holds the record as Japan's highest-grossing film, earning over 30 billion yen! 💸
+                This powerhouse has crafted twenty-two awe-inspiring films.
+                Think box office gold—most of their creations soared to the top
+                in Japan, and <strong>SPIRITED AWAY</strong>, directed by
+                Miyazaki in 2001, holds the record as Japan's highest-grossing
+                film, earning over 30 billion yen! 💸
               </p>
               <p className="px-4 mb-5 text-base leading-7 text-gray-500 dark:text-gray-400">
-                But it's not just about the numbers. Ghibli films are a magnet for awards and acclaim. <strong>SPIRITED AWAY</strong> clinched the Golden Bear at the 2002 Berlin International Film Festival and bagged the 2002 Academy Award for <strong>Best Animated Feature Film</strong>. Adding a touch of magic to reality, they teamed up to create the <strong>Ghibli Museum</strong> in 2001, a whimsical creation in Mitaka designed by the legendary Miyazaki.
+                But it's not just about the numbers. Ghibli films are a magnet
+                for awards and acclaim. <strong>SPIRITED AWAY</strong> clinched
+                the Golden Bear at the 2002 Berlin International Film Festival
+                and bagged the 2002 Academy Award for{" "}
+                <strong>Best Animated Feature Film</strong>. Adding a touch of
+                magic to reality, they teamed up to create the{" "}
+                <strong>Ghibli Museum</strong> in 2001, a whimsical creation in
+                Mitaka designed by the legendary Miyazaki.
               </p>
               <p className="px-4 mb-5 text-base leading-7 text-gray-500 dark:text-gray-400">
-                Zooming into the present, Studio Ghibli continues to dominate. Their last four films—<strong>THE WIND RISES (2013)</strong>, <strong>THE TALE OF THE PRINCESS KAGUYA (2013)</strong>, <strong>WHEN MARNIE WAS THERE (2014)</strong>, and <strong>THE RED TURTLE (2016)</strong>—nailed four consecutive Academy Award nominations.
+                Zooming into the present, Studio Ghibli continues to dominate.
+                Their last four films—<strong>THE WIND RISES (2013)</strong>,{" "}
+                <strong>THE TALE OF THE PRINCESS KAGUYA (2013)</strong>,{" "}
+                <strong>WHEN MARNIE WAS THERE (2014)</strong>, and{" "}
+                <strong>THE RED TURTLE (2016)</strong>—nailed four consecutive
+                Academy Award nominations.
                 <br></br>
-                And the magic doesn't stop—there's a new Ghibli masterpiece in the works!🌠
+                And the magic doesn't stop—there's a new Ghibli masterpiece in
+                the works!🌠
               </p>
             </div>
           </div>
