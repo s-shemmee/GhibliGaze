7dbb95a94f043fcf5d420b1a3999730a2d124e9b
shemmee <chaimalamirin@gmail.com>
2023-12-07 12:56:25 +0000

    Gallery Section

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,11 @@
 export default function Gallery() {
   return (
-    <section className="py-10 lg:py-10 bg-stone-100 dark:bg-gray-800" id="gallery-section">
+    <section className="py-10 lg:py-10 bg-stone-100" id="gallery">
       <div className="max-w-screen-xl py-4 mx-auto lg:py-6 md:px-6">
         {/* Section Title*/}
         <div className="text-center flex flex-col items-center mb-8">
-          <span className="text-sm text-gray-600 uppercase dark:text-gray-400">
-            Our
-          </span>
-          <h1 className="mt-2 text-3xl font-black font-oregano text-gray-700 md:text-5xl border-b-4 border-green-500 dark:text-gray-300">
+          <span className="text-sm text-gray-600 uppercase">Our</span>
+          <h1 className="mt-2 text-3xl font-black font-oregano text-gray-700 md:text-5xl border-b-4 border-green-500">
             Gallery
           </h1>
         </div>
