34ea3ef5e070c5d6ec756e483ea3880eb120fd89
shemmee <chaimalamirin@gmail.com>
2023-12-07 12:52:37 +0000

    Trailers Section

--- src/components/Trailers.jsx
+++ src/components/Trailers.jsx
@@ -1,42 +1,88 @@
-export default function Trailers() {
+import React from "react";
+
+// Trailer data array
+const trailersData = [
+  {
+    src: "https://www.youtube.com/embed/HaLISMAGdOE?si=dgxIEbsp-GZkJosJ;controls=0",
+    title: "MY NEIGHBOR TOTORO | Official English Trailer",
+  },
+  {
+    src: "https://www.youtube.com/embed/vf6c6n35wr4?si=CwF7i-PhXrm_1xtO;controls=0",
+    title: "PRINCESS MONONOKE | Official English Trailer",
+  },
+  {
+    src: "https://www.youtube-nocookie.com/embed/zaskbIZtfxQ?si=Xz0tbSrhUFwri392&amp;controls=0",
+    title: "NAUSICAÄ OF THE VALLEY OF THE WIND | Official English Trailer",
+  },
+  {
+    src: "https://www.youtube-nocookie.com/embed/5lMRfLJGXSM?si=t0bKCbbDu7ilZ6nk&amp;controls=0",
+    title: "CASTLE IN THE SKY | Official English Trailer",
+  },
+  {
+    src: "https://www.youtube-nocookie.com/embed/DJeUGpcle8s?si=K821BYCg5ViAOfmP&amp;controls=0",
+    title: "HOWL'S MOVING CASTLE | Official English Trailer",
+  },
+  {
+    src: "https://www.youtube-nocookie.com/embed/t6-fT0hjTvc?si=a_5W5crW5LGPG0W2&amp;controls=0",
+    title: "KIKI'S DELIVERY SERVICE | Official English Trailer",
+  },
+];
+
+// Common trailer styles
+const trailerStyles = "rounded-lg border-4 border-yellow-500";
+
+const Trailers = () => {
   return (
-    <section className="py-10 lg:py-10 bg-stone-100 dark:bg-gray-800" id="trailers-section">
+    <section
+      className="py-10 lg:py-10 bg-stone-100 relative overflow-hidden"
+      id="trailers"
+    >
       <div className="max-w-screen-xl py-4 mx-auto lg:py-6 md:px-6">
         {/* Section Title*/}
         <div className="text-center flex flex-col items-center mb-8">
-          <span className="text-sm text-gray-600 uppercase dark:text-gray-400">
-            Trailers
-          </span>
-          <h1 className="mt-2 text-3xl font-black font-oregano text-gray-700 md:text-5xl border-b-4 border-yellow-500 dark:text-gray-300">
+          <span className="text-sm text-gray-600 uppercase">Trailers</span>
+          <h1 className="mt-2 text-3xl font-black font-oregano text-gray-700 md:text-5xl border-b-4 border-yellow-500">
             Watch the Magic
           </h1>
         </div>
+
+        {/* Main Trailer */}
         <div className="grid gap-4 mt-8">
-          <div className="rounded-lg border-8 border-yellow-500">
-            <iframe width="100%" height="700" src="https://www.youtube.com/embed/HaLISMAGdOE?si=dgxIEbsp-GZkJosJ" title="MY NEIGHBOR TOTORO | Official English Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <div className={trailerStyles}>
+            <iframe
+              width="100%"
+              height="700"
+              src={trailersData[0].src}
+              title={trailersData[0].title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
           </div>
+
+          {/* Thumbnail Trailers */}
           <div className="grid grid-cols-5 gap-4">
-            <div className="rounded-lg border-4 border-yellow-500">
-              <iframe width="100%" height="100%" src="https://www.youtube.com/embed/vf6c6n35wr4?si=CwF7i-PhXrm_1xtO" title="PRINCESS MONONOKE | Official English Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-            <div className="rounded-lg border-4 border-yellow-500">
-              <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/zaskbIZtfxQ?si=Xz0tbSrhUFwri392&amp;controls=0" title="NAUSICAÄ OF THE VALLEY OF THE WIND | Official English Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-            <div className="rounded-lg border-4 border-yellow-500">
-              <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/5lMRfLJGXSM?si=t0bKCbbDu7ilZ6nk&amp;controls=0" title="CASTLE IN THE SKY | Official English Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-            <div className="rounded-lg border-4 border-yellow-500">
-              <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/DJeUGpcle8s?si=K821BYCg5ViAOfmP&amp;controls=0" title="HOWL'S MOVING CASTLE | Official English Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-            <div className="rounded-lg border-4 border-yellow-500">
-              <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/t6-fT0hjTvc?si=a_5W5crW5LGPG0W2&amp;controls=0" title="KIKI'S DELIVERY SERVICE | Official English Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
+            {trailersData.slice(1).map((trailer, index) => (
+              <div key={index} className={trailerStyles}>
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src={trailer.src}
+                  title={trailer.title}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ))}
           </div>
         </div>
+
         {/* Call-to-Action */}
         <div className="mt-8 text-center">
           <a
-            href="#"
+            href="https://youtube.com/playlist?list=PLrMoWLZPWpBU8qR-2hp90obIEEAelORR1&si=fWJkKYvSPOH5p2bu"
+            target="_blank"
             className="inline-block px-6 py-3 text-lg font-medium leading-6 text-center text-white transition duration-300 bg-yellow-500 rounded-md shadow hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
           >
             Watch More
@@ -46,3 +92,5 @@ export default function Trailers() {
     </section>
   );
 };
+
+export default Trailers;
