3fd4266b39e44538632ffe461985477aaa1a0c16
s-shemmee <chaimalamirin@gmail.com>
2024-06-27 17:36:37 +0100

    Add background audio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./globals.css";
+
+// Function to set the volume of the audio element
+function setAudioVolume(volume) {
+  const audio = document.getElementById("backgroundAudio");
+  if (audio) {
+    audio.volume = volume;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+setAudioVolume(0.2);
