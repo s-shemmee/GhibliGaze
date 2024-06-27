a2bc40140ce5049c7190e4f9b40cf3d6a250ecf9
s-shemmee <chaimalamirin@gmail.com>
2024-06-27 20:41:26 +0100

    Fix background audio autoplay issue and set volume

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./globals.css";
 
-// Function to set the volume of the audio element
-function setAudioVolume(volume) {
-  const audio = document.getElementById("backgroundAudio");
-  if (audio) {
-    audio.volume = volume;
-  }
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-setAudioVolume(0.2);
